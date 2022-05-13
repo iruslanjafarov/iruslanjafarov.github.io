2525e77d66aa578d47fdbd1da36f1279db123a2e
RuslanDurov <83516044+rusyaaaa@users.noreply.github.com>
2022-05-13 20:44:26 +0300

    Add files via upload

---
+++ src/js/script.js
@@ -0,0 +1,41 @@
+$(function() {
+    var title = document.title,
+      animSeq = ["|"],
+        animIndex = 0,
+        titleIndex = 0;
+    function doInverseSpinZeroPitch() {
+        var loadTitle = title.substring(0, titleIndex);
+        if (titleIndex > title.length) {
+            animIndex = 0;
+            titleIndex = 0
+        }
+        if (animIndex > 1) {
+            titleIndex++;
+            animIndex = 0
+        }
+        document.title = loadTitle + animSeq[0];
+        animIndex++
+    }
+    window.setInterval(doInverseSpinZeroPitch, 150);
+});
+let preloader = document.querySelector('.preloader')
+window.addEventListener('load', () => {
+    preloader.classList.add('hide')
+    setTimeout(() => {
+        preloader.remove()
+    }, 300);
+})
+document.onkeydown = function(e) {
+    if(event.keyCode == 123) {
+    return false;
+    }
+    if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)){
+    return false;
+    }
+    if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)){
+    return false;
+    }
+    if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)){
+    return false;
+    }
+}
